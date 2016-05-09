499ff0eb60fe6b9f85a78123d201e0c539e2f163
tsato1 <a8tb1117@gmail.com>
2016-05-08 18:37:13 -0700

    feat: Parcelable implemented for SuccessItem and TweetsItem

--- app/src/main/java/com/takahidesato/android/promatchandroid/ui/SuccessItem.java
+++ app/src/main/java/com/takahidesato/android/promatchandroid/ui/SuccessItem.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * Created by tsato on 4/26/16.
  */
-public class SuccessItem {
+public class SuccessItem implements Parcelable {
     public int id; // for this app
     public String idItem; // made by YouTube
     public String publishedAt;
@@ -36,8 +36,41 @@ public class SuccessItem {
         this.thumbnailMediumUrl = thumbnailMediumUrl;
     }
 
-//    private SuccessItem(Parcel in) {
-//        this.id = in.readInt();
-//
-//    }
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public SuccessItem createFromParcel(Parcel in) {
+            return new SuccessItem(in);
+        }
+
+        public SuccessItem[] newArray(int size) {
+            return new SuccessItem[size];
+        }
+    };
+
+    private SuccessItem(Parcel in) {
+        this.id = in.readInt();
+        this.idItem = in.readString();
+        this.publishedAt = in.readString();
+        this.channelId = in.readString();
+        this.title = in.readString();
+        this.description = in.readString();
+        this.thumbnailDefaultUrl = in.readString();
+        this.thumbnailMediumUrl = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flag) {
+        out.writeInt(this.id);
+        out.writeString(this.idItem);
+        out.writeString(this.publishedAt);
+        out.writeString(this.channelId);
+        out.writeString(this.title);
+        out.writeString(this.description);
+        out.writeString(this.thumbnailDefaultUrl);
+        out.writeString(this.thumbnailMediumUrl);
+    }
 }
--- app/src/main/java/com/takahidesato/android/promatchandroid/ui/TweetsItem.java
+++ app/src/main/java/com/takahidesato/android/promatchandroid/ui/TweetsItem.java
@@ -1,9 +1,12 @@
 package com.takahidesato.android.promatchandroid.ui;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by tsato on 4/26/16.
  */
-public class TweetsItem {
+public class TweetsItem implements Parcelable {
     public int id; // for this app
     public String idStr; // made by twitter
     public String createdAt;
@@ -31,5 +34,43 @@ public class TweetsItem {
         this.screenName = screenName;
         this.profileImageUrl = profileImageUrl;
         this.mediaImageUrl = mediaImageUrl;
+    }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator<TweetsItem>() {
+        public TweetsItem createFromParcel(Parcel in) {
+            return new TweetsItem(in);
+        }
+
+        public TweetsItem[] newArray(int size) {
+            return new TweetsItem[size];
+        }
+    };
+
+    private TweetsItem(Parcel in) {
+        this.id = in.readInt();
+        this.idStr = in.readString();
+        this.createdAt = in.readString();
+        this.text = in.readString();
+        this.name = in.readString();
+        this.screenName = in.readString();
+        this.profileImageUrl = in.readString();
+        this.mediaImageUrl = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flag) {
+        out.writeInt(this.id);
+        out.writeString(this.idStr);
+        out.writeString(this.createdAt);
+        out.writeString(this.text);
+        out.writeString(this.name);
+        out.writeString(this.screenName);
+        out.writeString(this.profileImageUrl);
+        out.writeString(this.mediaImageUrl);
     }
 }
