47ee34b9e853d4cc93b1f8048e22878bb5a1bd23
tsato1 <a8tb1117@gmail.com>
2016-06-23 19:58:55 -0700

    feat: runtime permission is implemented for api level 23 (below 23 still works)

--- app/src/main/java/com/takahidesato/android/promatchandroid/AboutActivity.java
+++ app/src/main/java/com/takahidesato/android/promatchandroid/AboutActivity.java
@@ -2,11 +2,15 @@ package com.takahidesato.android.promatchandroid;
 
 import android.Manifest;
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -26,6 +30,8 @@ import butterknife.OnClick;
  * Created by tsato on 5/23/16.
  */
 public class AboutActivity extends AppCompatActivity implements OnMapReadyCallback {
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 100;
+
     @Bind(R.id.txv_phone)
     TextView mPhoneTextView;
     @Bind(R.id.txv_email)
@@ -33,18 +39,7 @@ public class AboutActivity extends AppCompatActivity implements OnMapReadyCallba
     @OnClick(R.id.txv_phone)
     @TargetApi(Build.VERSION_CODES.M)
     public void onPhoneClick(View v) {
-        int currentApiVersion = android.os.Build.VERSION.SDK_INT;
-        if (currentApiVersion < 23) {
-            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + getString(R.string.promatch_phone)));
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        } else {
-            if (checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
-                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + getString(R.string.promatch_phone)));
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        }
+        checkPermission();
     }
     @OnClick(R.id.txv_email)
     public void onEmailClick(View v) {
@@ -76,4 +71,42 @@ public class AboutActivity extends AppCompatActivity implements OnMapReadyCallba
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
     }
 
+    /***
+     * Reference: https://inthecheesefactory.com/blog/things-you-need-to-know-about-android-m-permission-developer-edition/en
+     */
+    private void checkPermission() {
+        int hasCallPhonePermission = ContextCompat.checkSelfPermission(AboutActivity.this, Manifest.permission.CALL_PHONE);
+        if (hasCallPhonePermission != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(AboutActivity.this, Manifest.permission.CALL_PHONE)) {
+                showMessageOKCancel("You need to allow access to Phone",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(AboutActivity.this,
+                                        new String[] {Manifest.permission.CALL_PHONE},
+                                        REQUEST_CODE_ASK_PERMISSIONS);
+                            }
+                        });
+                return;
+            }
+            ActivityCompat.requestPermissions(AboutActivity.this,
+                    new String[] {Manifest.permission.CALL_PHONE},
+                    REQUEST_CODE_ASK_PERMISSIONS);
+            return;
+        }
+
+        /*** making a phone call after permission is granted ***/
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + getString(R.string.promatch_phone)));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(AboutActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
 }
